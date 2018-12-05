2a0d2b61344e055f37651708fe8a74ea9b838cfa
alex-vladut <alexandruvladut@gmail.com>
2018-12-05 23:44:16 +0200

    Added a simple test to prove that data can be stored to and retrieved from Cassandra

---
+++ online-shop/src/main/java/com/onlineshop/domain/product/Product.java
@@ -0,0 +1,50 @@
+package com.onlineshop.domain.product;
+
+import java.util.UUID;
+
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+@Table
+public class Product {
+
+    @PrimaryKey
+    @Column
+    private UUID id;
+    @Column
+    private String name;
+    @Column
+    private int price;
+
+    public Product(final UUID id, final String name, final int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+}
--- online-shop/src/test/java/com/onlineshop/OnlineShopApplicationTests.java
+++ online-shop/src/test/java/com/onlineshop/OnlineShopApplicationTests.java
@@ -1,16 +1,51 @@
 package com.onlineshop;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.cassandra.core.CassandraAdminOperations;
+import org.springframework.data.cassandra.core.cql.CqlIdentifier;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.onlineshop.domain.product.Product;
+import com.onlineshop.repository.ProductRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OnlineShopApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CassandraAdminOperations adminTemplate;
+
+    @Before
+    public void createTable() {
+        adminTemplate.createTable(true, CqlIdentifier.of("product"), Product.class, new HashMap<String, Object>());
+    }
+
+    @After
+    public void dropTable() {
+        adminTemplate.dropTable(CqlIdentifier.of("product"));
+    }
+
+    @Test
+    public void contextLoads() {
+        final Product product = new Product(UUID.randomUUID(), "Something", 100);
+        productRepository.save(product);
+
+        final Optional<Product> savedProduct = productRepository.findById(product.getId());
+        assertTrue(savedProduct.isPresent());
+    }
 
 }
