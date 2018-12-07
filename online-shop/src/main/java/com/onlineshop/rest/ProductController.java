92518113d0ab4833658c365431ced82b5c6a6284
alex-vladut <alexandruvladut@gmail.com>
2018-12-07 21:19:49 +0200

    Small fixes

--- online-shop/src/main/java/com/onlineshop/rest/ProductController.java
+++ online-shop/src/main/java/com/onlineshop/rest/ProductController.java
@@ -4,9 +4,6 @@ import static com.onlineshop.domain.Money.newMoney;
 import static com.onlineshop.domain.product.Product.newProduct;
 import static com.onlineshop.rest.dto.ProductDto.fromDomain;
 import static java.util.stream.Collectors.toList;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,8 +11,10 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,14 +28,14 @@ public class ProductController {
 	@Autowired
 	private ProductRepository productRepository;
 
-	@RequestMapping(path = "/products", method = GET)
+	@GetMapping("/products")
 	public List<ProductDto> getProducts() {
 		return productRepository.findAll().stream().map(ProductDto::fromDomain).collect(toList());
 	}
 
-	@RequestMapping(path = "/products/{productId}", method = GET)
-	public ResponseEntity<ProductDto> getProduct(@RequestParam final String productId) {
-		final Optional<Product> product = productRepository.findById(UUID.fromString(productId));
+	@GetMapping("/products/{productId}")
+	public ResponseEntity<ProductDto> getProduct(@RequestParam final UUID productId) {
+		final Optional<Product> product = productRepository.findById(productId);
 		if (product.isPresent()) {
 			return ResponseEntity.ok(fromDomain(product.get()));
 		} else {
@@ -44,16 +43,16 @@ public class ProductController {
 		}
 	}
 
-	@RequestMapping(path = "/products", method = POST)
+	@PostMapping("/products")
 	public ProductDto createProduct(@RequestBody final ProductDto productDto) {
 		final Product createdProduct = productRepository
 				.save(newProduct(productDto.name, newMoney(productDto.price.currency, productDto.price.amount)));
 		return fromDomain(createdProduct);
 	}
 
-	@RequestMapping(path = "/products", method = PUT)
+	@PutMapping("/products")
 	public ResponseEntity<ProductDto> updateProduct(@RequestBody final ProductDto productDto) {
-		final Optional<Product> productOptional = productRepository.findById(UUID.fromString(productDto.id));
+		final Optional<Product> productOptional = productRepository.findById(productDto.id);
 		if (productOptional.isPresent()) {
 			final Product product = productOptional.get();
 			product.update(productDto.name, newMoney(productDto.price.currency, productDto.price.amount));
--- online-shop/src/main/java/com/onlineshop/rest/dto/ProductDto.java
+++ online-shop/src/main/java/com/onlineshop/rest/dto/ProductDto.java
@@ -1,16 +1,18 @@
 package com.onlineshop.rest.dto;
 
+import java.util.UUID;
+
 import com.onlineshop.domain.product.Product;
 
 public class ProductDto {
 
-	public String id;
+	public UUID id;
 	public String name;
 	public MoneyDto price;
 
 	public static ProductDto fromDomain(Product product) {
 		final ProductDto productDto = new ProductDto();
-		productDto.id = product.id().toString();
+		productDto.id = product.id();
 		productDto.name = product.name();
 		productDto.price = MoneyDto.fromDomain(product.price());
 		return productDto;
